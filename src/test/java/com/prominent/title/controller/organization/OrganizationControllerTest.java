6a37b32dd210c8402fde142b569803a6e8904d06
dipaksimform <dipak.p@simformsolutions.com>
2022-11-16 14:04:25 +0530

    Updated code with minor changes.

--- src/test/java/com/prominent/title/controller/organization/OrganizationControllerTest.java
+++ src/test/java/com/prominent/title/controller/organization/OrganizationControllerTest.java
@@ -3,12 +3,14 @@ package com.prominent.title.controller.organization;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prominent.title.dto.organization.*;
 import com.prominent.title.dto.user.UserIdAndNameDto;
+import com.prominent.title.exception.OrganizationNotFoundException;
 import com.prominent.title.service.organization.OrganizationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -50,6 +52,22 @@ class OrganizationControllerTest {
     }
 
     @Test
+    void add_organization_already_exist_test_409_conflict() throws Exception {
+
+        OrganizationDto organizationDto = new OrganizationDto(1, "Thurston Wyatt real Estate LLC.", "TestType", "New York", "New York", "New York", "1111", "Jcob", "2025550162", "jcob@gmail.com", "30-2232116", "2");
+        PaymentInformationDto paymentInformationDto = new PaymentInformationDto("Cash", "John Patel", "2472 Thunder Road", "Anchorage", "Anchorage", "99501", "123456789101112", "1020304050");
+        OrganizationEntryDto organizationEntryDto = new OrganizationEntryDto(organizationDto, paymentInformationDto);
+
+        when(organizationService.add(any())).thenThrow(DataIntegrityViolationException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/organization/entry")
+                        .content(objectMapper.writeValueAsString(organizationEntryDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict());
+    }
+
+    @Test
     void edit_organization_test_200_ok() throws Exception {
 
         OrganizationDto organizationDto = new OrganizationDto(1, "Thurston Wyatt real Estate LLC.", "TestType", "New York", "New York", "New York", "1111", "Surya", "2025550162", "Surya@gmail.com", "30-2232116", "2");
@@ -79,6 +97,18 @@ class OrganizationControllerTest {
     }
 
     @Test
+    void delete_organization_test_404_not_found() throws Exception {
+
+        when(organizationService.deleteOrganization(anyInt())).thenThrow(OrganizationNotFoundException.class);
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/organization/delete")
+                        .param("organizationId", "1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+
+    }
+
+    @Test
     void get_organization_test_200_ok() throws Exception {
 
         OrganizationDto organizationDto = new OrganizationDto(1, "Thurston Wyatt real Estate LLC.", "TestType", "New York", "New York", "New York", "1111", "Jcob", "2025550162", "jcob@gmail.com", "30-2232116", "2");
@@ -94,6 +124,25 @@ class OrganizationControllerTest {
                         .param("organizationId", "1")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void get_organization_test_404_not_found() throws Exception {
+
+        OrganizationDto organizationDto = new OrganizationDto(1, "Thurston Wyatt real Estate LLC.", "TestType", "New York", "New York", "New York", "1111", "Jcob", "2025550162", "jcob@gmail.com", "30-2232116", "2");
+        PaymentInformationDto paymentInformationDto = new PaymentInformationDto("Cash", "John Patel", "2472 Thunder Road", "Anchorage", "Anchorage", "99501", "123456789101112", "1020304050");
+        List<UserIdAndNameDto> organizationBrokers = new ArrayList<>();
+        organizationBrokers.add(new UserIdAndNameDto(10, "Virat", "Kohli", "virat@gmail.com", "8605588552"));
+
+        OrganizationDtoWithBrokers organizationDtoWithBrokers = new OrganizationDtoWithBrokers(organizationDto, paymentInformationDto, organizationBrokers);
+        when(organizationService.getOrganization(anyInt())).thenThrow(OrganizationNotFoundException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/organization/get")
+                        .param("organizationId", "1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
 
     }
 
--- src/test/java/com/prominent/title/controller/signup/SignupControllerTest.java
+++ src/test/java/com/prominent/title/controller/signup/SignupControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -41,7 +42,19 @@ class SignupControllerTest {
     }
 
     @Test
-    void user_sign_up_test_404_not_found() throws Exception {
+    void user_sign_up_username_exist_test_409_conflict() throws Exception {
+        UserSignupDto userSignupDto = new UserSignupDto("dipak@gmail.com", "9963725849", "User", "Test@1231111", "Test@1231111");
+        when(userService.addUser(any(), any())).thenThrow(DataIntegrityViolationException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/signup/user")
+                        .content(objectMapper.writeValueAsString(userSignupDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    void user_sign_up_test_404_role_not_found() throws Exception {
         UserSignupDto userSignupDto = new UserSignupDto("dipak@gmail.com", "9963725849", "User", "Test@1231111", "Test@1231111");
         when(userService.addUser(any(), any())).thenThrow(RoleNotFoundException.class);
 
@@ -51,4 +64,6 @@ class SignupControllerTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
     }
+
+
 }
